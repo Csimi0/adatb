5a989462d5c0d23386af8f0104081afaaa14b508
Vincze Imre Bence <h166176@stud.u-szeged.hu>
2022-11-26 16:32:38 +0100

    Initial commit

---
+++ src/test/java/com/example/adatb/AdatbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adatb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdatbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
